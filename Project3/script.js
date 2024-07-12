b4046f4779a0ede14266de09bf06124c4d22992a
bmayoumgc <141936281+bmayoumgc@users.noreply.github.com>
2024-07-12 13:19:05 -0400

    Add files via upload

--- Project3/script.js
+++ Project3/script.js
@@ -1,9 +1,6 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function Nav() {
-    var state = document.getElementsByTagName("nav")[0].style.display;
-    if (state == 'block') {
-        document.getElementsByTagName("nav")[0].style.display = 'none';
-    } else {
-        document.getElementsByTagName("nav")[0].style.display = 'block';
-    }
+
+function NAV() {
+    var nav = document.querySelector('.nav');
+    nav.classList.toggle('nav-visible'); // Toggle the visibility of the navigation menu
 }
