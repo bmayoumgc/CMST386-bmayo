d2e23780eb066fb2379f06eaa8c2d496b68ddbbc
bmayoumgc <141936281+bmayoumgc@users.noreply.github.com>
2024-08-04 16:43:59 -0400

    Add files via upload

--- project4/script.js
+++ project4/script.js
@@ -4,3 +4,21 @@ function NAV() {
     var nav = document.querySelector('.nav');
     nav.classList.toggle('nav-visible'); // Toggle the visibility of the navigation menu
 }
+
+const name = document.getElementById('name')
+const name = document.getElementById('email')
+const name = document.getElementById('number')
+const name = document.getElementById('form')
+const errorElement = docucument.getElementById('error')
+
+Form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null) {
+        message.push('Name is required')
+    }
+
+    if (message.lehgth > 0) {
+    e.preventDefault()
+    errorElement.innerText = message.join(', ')
+    }
+})
